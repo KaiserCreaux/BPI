5d7022a84a13b2f463b62e071b3170a0f524369e
KaiserCreaux <creaux@gmail.com>
2014-09-18 12:44:13 -0400

    Approaching purpose

---
+++ sentences.js
@@ -0,0 +1,49 @@
+var xmlHttp = createXmlHttpRequestObject();
+
+function createXmlHttpRequestObject() {
+
+	var xmlHttp;
+
+	if (window.ActiveXObject){
+		try{
+			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+		} catch (e) {
+			xmlHttp = false;
+		}
+	}else{
+		try{
+			xmlHttp = new XMLHttpRequest();
+		} catch (e) {
+			xmlHttp = false;
+		}
+	}
+
+	if (!xmlHttp) {
+		alert("Could not create XML Object");
+	} else {
+		return xmlHttp;
+	}
+}
+
+function process() {
+
+
+		food = encodeURIComponent(document.getElementById("userInput").value);
+		//alert(food);
+		xmlHttp.open("GET", "sentences.php?food="+food, true);
+		xmlHttp.onreadystatechange = handleServerResponse;
+		xmlHttp.send();
+
+}
+
+function handleServerResponse () {
+
+	if ( xmlHttp.readyState==4 )
+		if ( xmlHttp.status==200) {
+		xmlResponse = xmlHttp.responseXML;
+		xmlDocumentElement = xmlResponse.documentElement;
+		message = xmlDocumentElement.firstChild.textContent;
+		document.getElementById("underInput").innerHTML = '<span style="color:blue">' + message + '</span>';
+	}
+}
+
--- sentences.php
+++ sentences.php
@@ -3,13 +3,14 @@ header('Content-Type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
 
 echo '<response>';
-	$food = $_get['food'];
+	$food = $_GET['food'];
 	$foodArray = array('banana', 'jerky', 'beef', 'loaf', 'ham');
-	if(in_array($food,$foodArray))
+	if(in_array($food,$foodArray)){
 		echo 'We do have '.$food.'!';
-	elseif($food=='')
+	} elseif($food=='') {
 		echo 'Enter a food you idiot';
-	else
-		echo 'Sorry punk, we don't sell no '.$food.'!';
+	} else {
+		echo 'Sorry punk, we dont sell no '.$food.'!';
+	}
 echo '</response>';
 ?>
